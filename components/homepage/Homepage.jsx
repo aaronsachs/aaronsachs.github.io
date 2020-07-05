8d9dcfd7546c2079184f03913cf42986f631c972
Aaron Sachs <aaron@simplebet.io>
2020-07-05 00:40:56 -0400

    convert to React

---
+++ components/homepage/Homepage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Homepage.css';
+
+class Homepage extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	render() {
+		return (
+			<div>
+			  This website is under construction
+			</div>
+		)
+	}
+}
+
+export default Homepage;
